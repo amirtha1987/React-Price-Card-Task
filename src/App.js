f12054f601d9821a499ec2e334c4bb852a443e43
VijayMayhul <vijaymayhul@gmail.com>
2024-01-13 00:33:38 +0530

    Adding React-Price-Card Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import Card from './Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='h1 text-capitalize text-center my-2 text-primary'>React Price Card Task</h1>
+      <div className='row bg-primary my-2 pt-3 px-3 d-flex justify-content-center'>
+        <Card name={{type:'FREE', rate:0, 
+        icon:["fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", 
+        "fa-solid fa-xmark", "fa-solid fa-xmark", "fa-solid fa-xmark", "fa-solid fa-xmark"]}}></Card>
+        <Card name={{type:'PLUS', rate:9, icon:["fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", 
+        "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-xmark"]}}></Card>
+        <Card name={{type:'PRO', rate:49, icon:["fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", 
+        "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check", "fa-solid fa-check"]}}></Card>
+      </div>
     </div>
   );
 }
