24528d9a03366813a49ce242ad9be40c9d59f215
VijayMayhul <vijaymayhul@gmail.com>
2024-01-18 14:36:01 +0530

    Updating Price Card Files

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
 function Card(props){
-    let pContent = ["Single User", "50GB Storage", "Unlimited Public Projects", "Community Access",
-"Unlimited Private Projects",  "Dedicated Phone Support", "Free Sub-domain"];
     return (
         <div className="col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-3">
             <div className="card">
                 <div className="card-header text-center">
-                    <span className="fw-bold opacity-25">{props.name.type}</span>
-                    <h2>${props.name.rate}/month</h2>
+                    <span className="fw-bold opacity-50 text-uppercase">{props.name.planType}</span>
+                    <h2 className='h2 my-2'>${props.name.planRate}/month</h2>
                 </div>
                 <div className="card-body">
-                {props.name.icon.map((iconClass, index) => (
-                    <p key={index} className={`card-text ${iconClass.includes('fa-xmark') ? 'opacity-25' : ''}`}>
-                    <i className={`${iconClass}`}></i> {index === 7 ? 'Monthly Status Reports' : pContent[index]}
-                    </p>
-                ))}
+                    {
+                        props.name.features.map((ele, index)=>{
+                            return <p key={index} className={`card-text ${ele.enabled ? '' : 'opacity-25'}`}>
+                                <FontAwesomeIcon icon={ele.enabled ? faCheck : faTimes} />
+                                {/* <i className={`fas ${ele.enabled ? 'fa-check' : 'fa-times text-danger'}`}></i> */}
+                                {ele.name}
+                            </p>
+                        })
+                    }
                     
                     <div className="d-flex justify-content-center">
-                        <a href="#" className="w-100 btn btn-primary text-uppercase fw-bold py-3">Button</a>
+                        <a href="#" className={`w-100 btn btn-primary text-uppercase fw-bold py-3 
+                        ${
+                            props.name.planType !== 'Pro'
+                            ? props.name.planType === 'Plus' ? 'opacity-75' : 'opacity-50'
+                            : ''
+                        }`
+                        }>Button</a>
                     </div>
                 </div>
             </div>
@@ -33,4 +46,12 @@ export default Card;
 <p className="card-text"><i className={`${props.name.icon[4]}`}></i> Unlimited Private Projects</p>
 <p className="card-text"><i className={`${props.name.icon[5]}`}></i> Dedicated Phone Support</p>
 <p className="card-text"><i className={`${props.name.icon[6]}`}></i> Free Sub-domain</p>
-<p className="card-text"><i className={`${props.name.icon[7]}`}></i> Monthly Status Reports</p> */}
+<p className="card-text"><i className={`${props.name.icon[7]}`}></i> Monthly Status Reports</p>
+
+{props.name.icon.map((iconClass, index) => (
+                    <p key={index} className={`card-text ${iconClass.includes('fa-xmark') ? 'opacity-25' : ''}`}>
+                    <i className={`${iconClass}`}></i> {index === 7 ? 'Monthly Status Reports' : pContent[index]}
+                    </p>
+                ))}
+
+*/}
